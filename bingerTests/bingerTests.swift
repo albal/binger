20aeeed3ca16d0468b1dd74ece33755ef4943f94
Al West <al@tsew.net>
2026-06-22 15:43:26 +0100

    Initial Commit

---
+++ bingerTests/bingerTests.swift
@@ -0,0 +1,19 @@
+//
+//  bingerTests.swift
+//  bingerTests
+//
+//  Created by Al West on 22/06/2026.
+//
+
+import Testing
+@testable import binger
+
+struct bingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
